4066678c7c12b01d5353e5621d12eea69e8dec64
Вадим <43517794+VadimkaG@users.noreply.github.com>
2019-09-05 15:25:27 +0300

    Delete GameStartException.java

--- client/GameStartException.java
+++
@@ -1,8 +0,0 @@
-package ru.vadimka.nfswlauncher.client;
-
-public class GameStartException extends Exception {
-	private static final long serialVersionUID = 1866904255945531404L;
-	public GameStartException(String string) {
-		super(string);
-	}
-}
